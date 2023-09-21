6ef8f9c98270ddaf6f470a729bdee7cdd4aab6c8
Davincy Project <bdavincy.bd@gmail.comm>
2023-09-21 19:31:29 +0700

    tweaks: change Button Component bgColor to fix no unknown property

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,11 @@ import PropTypes from "prop-types";
 export default function Button(props) {
   const { bgColor, text, type, onClick } = props;
 
-  const className = `${
-    bgColor === undefined ? "bg-btn-default" : bgColor
-  } rounded mt-2 text-white font-medium w-full h-7`;
+  const className = `${bgColor === undefined ? "bg-btn-default" : bgColor
+    } rounded mt-2 text-white font-medium w-full h-7`;
 
   return (
     <button
-      // eslint-disable-next-line react/no-unknown-property
-      bgColor={bgColor}
       onClick={onClick}
       type={type}
       className={className}
